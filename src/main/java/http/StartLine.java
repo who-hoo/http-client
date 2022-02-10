8a22365c25acbd249a435555868bbfbce40ba4ea
who-hoo <who.ho3ov@gmail.com>
2022-02-11 02:47:14 +0900

    :sparkles: HTTP 요청, 응답의 시작 줄 추상클래스 작성

---
+++ src/main/java/http/StartLine.java
@@ -0,0 +1,9 @@
+package http;
+
+public abstract class StartLine {
+
+    protected final String SP = " ";
+    protected final String CRLF = "\r\n";
+
+    protected String httpVersion = "HTTP/1.1";
+}
