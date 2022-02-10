decedb57b207d17ebc559372c4408fd14dbeddff
who-hoo <who.ho3ov@gmail.com>
2022-02-10 17:38:17 +0900

    :sparkles: HTTP 요청 메서드를 정의한 Enum HttpMethod 작성

---
+++ src/main/java/request/HttpMethod.java
@@ -0,0 +1,23 @@
+package request;
+
+public enum HttpMethod {
+    GET("GET"),
+    HEAD("HEAD"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE"),
+    CONNECT("CONNECT"),
+    OPTIONS("POST"),
+    TRACE("TRACE"),
+    PATCH("PATCH");
+
+    private final String value;
+
+    HttpMethod(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
