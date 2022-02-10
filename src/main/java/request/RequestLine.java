4515cc349a18a7f720dcabc6db3ff96900e332bb
who-hoo <who.ho3ov@gmail.com>
2022-02-10 23:08:36 +0900

    :recycle: requestTarget 설정하는 부분 개선
    - 중복 제거를 위한 메서드 분리
    - query가 null일 경우 null 대신 빈 문자열 리턴
    - requestTarget 전체가 빈 문자열일 경우 "/" 리턴

--- src/main/java/request/RequestLine.java
+++ src/main/java/request/RequestLine.java
@@ -11,14 +11,19 @@ public class RequestLine {
     private final String requestTarget;
     private String httpVersion = "HTTP/1.1";
 
+    private String getRequestTarget(URL url) {
+        String requestTarget = url.getPath() + ((url.getQuery() == null) ? "" : url.getQuery());
+        return requestTarget.isBlank() ? "/" : requestTarget;
+    }
+
     public RequestLine(URL url, HttpMethod method) {
         this.method = method;
-        this.requestTarget = url.getPath() + url.getFile() + url.getQuery();
+        this.requestTarget = getRequestTarget(url);
     }
 
     public RequestLine(URL url, HttpMethod method, String httpVersion) {
         this.method = method;
-        this.requestTarget = url.getPath() + url.getFile() + url.getQuery();
+        this.requestTarget = getRequestTarget(url);
         this.httpVersion = httpVersion;
     }
 
