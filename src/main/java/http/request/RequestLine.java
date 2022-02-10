4ce1bd058eb1dc5f567ff39586906923fbda7ac1
who-hoo <who.ho3ov@gmail.com>
2022-02-11 02:47:53 +0900

    :recycle: 추상클래스 StartLine을 상속받도록 수정

--- src/main/java/http/request/RequestLine.java
+++ src/main/java/http/request/RequestLine.java
@@ -1,16 +1,13 @@
 package http.request;
 
 import http.HttpMethod;
+import http.StartLine;
 import java.net.URL;
 
-public class RequestLine {
-
-    private final String SP = " ";
-    private final String CRLF = "\r\n";
+public class RequestLine extends StartLine {
 
     private final HttpMethod method;
     private final String requestTarget;
-    private String httpVersion = "HTTP/1.1";
 
     private String getRequestTarget(URL url) {
         String requestTarget = url.getPath() + ((url.getQuery() == null) ? "" : url.getQuery());
